051d8d56242c314aecc97a60ad3d97affb27b249
Joao Lima <jpclima93@gmail.com>
2021-11-16 22:08:34 +0100

    Create CRUD operations

--- controllers/task-controller.js
+++ controllers/task-controller.js
@@ -8,10 +8,91 @@ exports.getAllTasks = async (req, res) => {
     // SEND RESPONSE
     res.status(200).json({
       status: 'success',
-      tasks,
+      data: {
+        tasks,
+      },
     });
   } catch (err) {
     res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.getTask = async (req, res) => {
+  try {
+    // EXECUTE QUERY
+    const task = await Task.findById(req.params.id);
+
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      data: {
+        task,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.createTask = async (req, res) => {
+  try {
+    // EXECUTE QUERY
+    const newTask = await Task.create(req.body);
+
+    // SEND RESPONSE
+    res.status(201).json({
+      status: 'success',
+      data: {
+        task: newTask,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.updateTask = async (req, res) => {
+  try {
+    // EXECUTE QUERY
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      task,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.deleteTask = async (req, res) => {
+  try {
+    // EXECUTE QUERY
+    await Task.findByIdAndDelete(req.params.id);
+
+    // SEND RESPONSE
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(500).json({
       status: 'fail',
       message: err,
     });
--- routes/task-router.js
+++ routes/task-router.js
@@ -3,6 +3,15 @@ const taskController = require('../controllers/task-controller');
 
 const router = express.Router();
 
-router.route('/').get(taskController.getAllTasks);
+router
+  .route('/')
+  .get(taskController.getAllTasks)
+  .post(taskController.createTask);
+
+router
+  .route('/:id')
+  .get(taskController.getTask)
+  .patch(taskController.updateTask)
+  .delete(taskController.deleteTask);
 
 module.exports = router;
