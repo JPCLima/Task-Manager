084a83e8b49506ad40c651654cdbf1cfc09088b1
Joao Lima <jpclima93@gmail.com>
2021-11-16 17:20:19 +0100

    Create controllers

---
+++ controllers/task-controller.js
@@ -0,0 +1,12 @@
+exports.getAllTasks = async (req, res) => {
+  try {
+    res.status(200).json({
+      status: 'success',
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
