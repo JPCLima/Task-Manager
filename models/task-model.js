c7587b78bc9a65bbf337bac180ef823ed685ad7d
Joao Lima <jpclima93@gmail.com>
2021-11-16 21:32:51 +0100

    Create schema for task

---
+++ models/task-model.js
@@ -0,0 +1,34 @@
+/* eslint-disable new-cap */
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'A task name is required'],
+  },
+  priority: {
+    type: Number,
+    default: 4,
+    minimum: [1, 'priority must to be above 1'],
+    maximum: [4, 'priority must be bellow 4'],
+  },
+  status: {
+    type: Boolean,
+    default: false,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date.now(),
+  },
+  completionDate: {
+    type: Date,
+  },
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
